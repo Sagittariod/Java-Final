4ed3cb3ed2ea9b5a3ce08fcd90396819b06c26b6
Sagittariod <146118746+Sagittariod@users.noreply.github.com>
2024-05-23 12:38:51 -0400

    Fixed existing bugs, *should* be futureproof

--- MultipleChoice.java
+++ MultipleChoice.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-import javax.swing.*;
-import java.io.*;
 
 public class MultipleChoice 
 {
@@ -9,11 +7,11 @@ public class MultipleChoice
     {
         Scanner scan = new Scanner(System.in);
         
-        String[] trueAnswers = {"a","c"};
-        ArrayList<String> userAnswers = new ArrayList();
-        String[] questions = {"This is the most common species of primates.", "This primate is the largest of them all."};
-        String[] potentialChoices = {"a) Humans, b) Neanderthals, c) Spider Monkeys, d) Lemurs", "a) Baboons, b) Orangutans, c) Gorillas, d) Mandrills"};
-        String[] generateHint = {"This species is most likely the one currently taking this quiz.", "The scientific name of this species is also the name they are typically and informally called."};
+        String[] trueAnswers = {"a", "c", "a"};
+        ArrayList<String> userAnswers = new ArrayList<>();
+        String[] questions = {"This is the most common species of primates.", "This primate is the largest of them all.", "This species is a great ape with the scientific name 'Pongo'."};
+        String[] potentialChoices = {"a) Humans, b) Neanderthals, c) Spider Monkeys, d) Lemurs", "a) Baboons, b) Orangutans, c) Gorillas, d) Mandrills", "a) Orangutans, b) Strepsirrhini, c) Lemurs, d) Chimpanzees"};
+        String[] generateHint = {"This species is most likely the one currently taking this quiz.", "The scientific name of this species is also the name they are typically and informally called.", "This species is known for its orange appearance."};
         
         int score = 0;
         
@@ -22,31 +20,29 @@ public class MultipleChoice
         scan.next();
         
 
-        for(int i = 0; i < trueAnswers.length; i++)
+        for(int i = 0; i < questions.length; i++)
         {
             System.out.println(questions[i]);
             System.out.println(potentialChoices[i]);
             String tempAnswer = scan.next();
             userAnswers.add(tempAnswer);
             
-            if(userAnswers.get(i).equalsIgnoreCase("Hint"))
+            while (userAnswers.get(i).equalsIgnoreCase("Hint"))
             {
                 System.out.println(generateHint[i]);
+                System.out.println("Please enter your answer again:");
                 tempAnswer = scan.next();
-                userAnswers.add(tempAnswer);
+                userAnswers.set(i, tempAnswer); // Update the user's answer in the list
             }
-            else if(userAnswers.get(i).equalsIgnoreCase(trueAnswers[i]))
+            
+            if(userAnswers.get(i).equalsIgnoreCase(trueAnswers[i]))
             {
                 score++;
             }
-            else
-            {
-                
-            }
-            
         }
 
-        System.out.println("Final Score: " + score + " out of 10. Press any key to exit.");
+        System.out.println("Final Score: " + score + " out of " + questions.length);
+        System.out.println("Enter any combination of characters to exit.");
         scan.next();
     }
 
